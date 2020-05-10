dd6d7b946f7ecde77773d1cfa900cc2bf3100d26
shakti-garg <shakti.garg@gmail.com>
2020-05-10 14:16:12 +0530

    add tags and context to span created in consumer

--- opentracing_kafka/tracing_kafka_consumer.py
+++ opentracing_kafka/tracing_kafka_consumer.py
@@ -1,4 +1,5 @@
 from confluent_kafka.cimpl import Consumer
+from opentracing import Format, tags
 
 
 class TracingKafkaConsumer(Consumer):
@@ -8,5 +9,26 @@ class TracingKafkaConsumer(Consumer):
         self.tracer = tracer
 
     def poll(self, timeout=None):
-        with self.tracer.start_span('ConsumerSpan') as span:
-            Consumer.poll(self, timeout)
+        msg = Consumer.poll(self, timeout)
+
+        if msg is not None:
+            self.build_and_finish_child_span(msg)
+
+        return msg
+
+    def build_and_finish_child_span(self, msg):
+        parent_context = self.tracer.extract(Format.TEXT_MAP, dict(msg.headers()))
+        consumer_oper = "From_" + msg.topic()
+        consumer_tags = {tags.SPAN_KIND: tags.SPAN_KIND_CONSUMER,
+                         tags.COMPONENT: 'python-kafka', tags.PEER_SERVICE: 'kafka',
+                         tags.MESSAGE_BUS_DESTINATION: msg.topic(),
+                         'partition': msg.partition(), 'offset': msg.offset()
+                         }
+
+        span = self.tracer.start_span(consumer_oper, child_of=parent_context, tags=consumer_tags)
+        span.finish()
+
+        # Inject created span context into message header for extraction by client to continue span chain
+        msg_header_dict = dict(msg.headers())
+        self.tracer.inject(span.context, Format.TEXT_MAP, msg_header_dict)
+        msg.set_headers(list(msg_header_dict.items()))
