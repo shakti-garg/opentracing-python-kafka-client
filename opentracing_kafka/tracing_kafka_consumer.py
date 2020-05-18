44fc75236e7cd8ed703c5dc79298ec00f3a852c9
shakti-garg <shakti.garg@gmail.com>
2020-05-18 07:34:17 +0530

    add option to provide custom span_name_provider and span_tags_providers

--- opentracing_kafka/tracing_kafka_consumer.py
+++ opentracing_kafka/tracing_kafka_consumer.py
@@ -1,14 +1,55 @@
 from confluent_kafka.cimpl import Consumer
 from opentracing import Format, tags, follows_from
 
+from utils import merge_two_dicts
+
+
+def default_span_name_provider(msg):
+    return "From_" + msg.topic()
+
+
+def consumer_span_tags_provider(msg):
+    return {
+        tags.SPAN_KIND: tags.SPAN_KIND_CONSUMER,
+        tags.COMPONENT: 'python-kafka',
+        tags.PEER_SERVICE: 'kafka',
+        tags.MESSAGE_BUS_DESTINATION: msg.topic(),
+        'partition': msg.partition(),
+        'offset': msg.offset()
+    }
+
 
 class TracingKafkaConsumer(Consumer):
 
-    def __init__(self, config, tracer):
+    def __init__(self, config, tracer, span_name_provider=default_span_name_provider, span_tags_providers=[]):
+        """
+        constructor method
+
+        :param config: dictionary of configurations for kafka producer
+        :param tracer: instance of tracer-client
+        :param span_name_provider: [optional] function returning span-name
+        sample function def::
+            def func(msg):
+                return ''
+
+        :param span_tags_providers: [optional] list of functions where each function returns dictionary of tags
+        sample function def::
+            def func(msg):
+                return {}
+        """
         super().__init__(config)
+
         self.tracer = tracer
+        self.span_name_provider = span_name_provider
+        self.span_tags_providers = span_tags_providers
 
     def poll(self, timeout=None):
+        """
+        overridden method
+
+        :param timeout:
+        :return:
+        """
         msg = Consumer.poll(self, timeout)
 
         if msg is not None:
@@ -17,6 +58,14 @@ class TracingKafkaConsumer(Consumer):
         return msg
 
     def consume(self, num_messages=1, *args, **kwargs):
+        """
+        overridden method
+
+        :param num_messages:
+        :param args:
+        :param kwargs:
+        :return:
+        """
         msgs = Consumer.consume(self, num_messages, *args, **kwargs)
 
         for msg in msgs:
@@ -29,14 +78,13 @@ class TracingKafkaConsumer(Consumer):
         msg_header_text_dict = dict((key, binary_value.decode("utf-8")) for key, binary_value in msg.headers())
 
         parent_context = self.tracer.extract(Format.TEXT_MAP, msg_header_text_dict)
-        consumer_oper = "From_" + msg.topic()
-        consumer_tags = {tags.SPAN_KIND: tags.SPAN_KIND_CONSUMER,
-                         tags.COMPONENT: 'python-kafka', tags.PEER_SERVICE: 'kafka',
-                         tags.MESSAGE_BUS_DESTINATION: msg.topic(),
-                         'partition': msg.partition(), 'offset': msg.offset()
-                         }
 
-        span = self.tracer.start_span(consumer_oper, references=follows_from(parent_context), tags=consumer_tags)
+        user_tags = {}
+        for span_tag_provider in self.span_tags_providers:
+            user_tags = merge_two_dicts(user_tags, span_tag_provider(msg))
+
+        span = self.tracer.start_span(self.span_name_provider(msg), references=[follows_from(parent_context)],
+                                      tags=merge_two_dicts(consumer_span_tags_provider(msg), user_tags))
         span.finish()
 
         # Inject created span context into message header for extraction by client to continue span chain
--- opentracing_kafka/tracing_kafka_producer.py
+++ opentracing_kafka/tracing_kafka_producer.py
@@ -3,14 +3,30 @@ import traceback
 from confluent_kafka.cimpl import Producer
 from opentracing import tags, Format, follows_from
 
+from utils import merge_two_dicts
+
+
+def default_span_name_provider(topic, key, value):
+    return "To_" + topic
+
+
+def producer_span_tags_provider(topic):
+    return {
+        tags.SPAN_KIND: tags.SPAN_KIND_PRODUCER,
+        tags.COMPONENT: 'python-kafka',
+        tags.PEER_SERVICE: 'kafka',
+        tags.MESSAGE_BUS_DESTINATION: topic
+    }
+
 
 def error_logs(err):
-    return {'event': tags.ERROR,
-            'error.kind': err.__class__,
-            'error.object': err,
-            'error.message': str(err),
-            'error.stack': traceback.extract_tb(err.__traceback__)
-            }
+    return {
+        'event': tags.ERROR,
+        'error.kind': err.__class__,
+        'error.object': err,
+        'error.message': str(err),
+        'error.stack': traceback.extract_tb(err.__traceback__)
+    }
 
 
 # curried callback function to wrap original 'on_delivery' callback
@@ -19,6 +35,10 @@ def create_tracing_delivery_callback(on_delivery_fn, span):
         if err is not None:
             span.set_tag('error', 'true')
             span.log_kv(error_logs(err))
+
+        if msg is not None:
+            span.set_tag('partition', msg.partition())
+            span.set_tag('offset', msg.offset())
 
         if on_delivery_fn is not None:
             on_delivery_fn(err, msg)
@@ -30,21 +50,49 @@ def create_tracing_delivery_callback(on_delivery_fn, span):
 
 class TracingKafkaProducer(Producer):
 
-    def __init__(self, config, tracer):
+    def __init__(self, config, tracer, span_name_provider=default_span_name_provider, span_tags_providers=[]):
+        """
+        constructor method
+
+        :param config: dictionary of configurations for kafka producer
+        :param tracer: instance of tracer-client
+        :param span_name_provider: [optional] function returning span-name
+        sample function def::
+            def func(topic, key, value):
+                return ''
+
+        :param span_tags_providers: [optional] list of functions where each function returns dictionary of tags
+        sample function def::
+            def func(topic, key, value):
+                return {}
+        """
         super().__init__(config)
+
         self.tracer = tracer
+        self.span_name_provider = span_name_provider
+        self.span_tags_providers = span_tags_providers
 
     def produce(self, topic, value=None, *args, **kwargs):
+        """
+        overridden method
+
+        :param topic:
+        :param value:
+        :param args:
+        :param kwargs:
+        :return:
+        """
         msg_header_dict = dict() if kwargs.get('headers') is None else dict(kwargs.get('headers'))
 
         parent_context = self.tracer.extract(Format.TEXT_MAP, msg_header_dict)
-        producer_oper = "To_" + topic
-        producer_tags = {tags.SPAN_KIND: tags.SPAN_KIND_PRODUCER,
-                         tags.COMPONENT: 'python-kafka', tags.PEER_SERVICE: 'kafka',
-                         tags.MESSAGE_BUS_DESTINATION: topic
-                         }
 
-        span = self.tracer.start_span(producer_oper, references=follows_from(parent_context), tags=producer_tags)
+        user_tags = {}
+        for span_tag_provider in self.span_tags_providers:
+            user_tags = merge_two_dicts(user_tags, span_tag_provider(topic, kwargs.get('key'), value))
+
+        span = self.tracer.start_span(self.span_name_provider(topic, kwargs.get('key'), value),
+                                      references=[follows_from(parent_context)],
+                                      tags=merge_two_dicts(producer_span_tags_provider(topic), user_tags))
 
         # Inject created span context into message header for sending to kafka queue
         self.tracer.inject(span.context, Format.TEXT_MAP, msg_header_dict)
